f154f4d81b6c916e888b7a670953a16c38e1efa2
cmdcjones <43971640+cmdcjones@users.noreply.github.com>
2021-12-10 10:24:32 -0500

    updated script, added features

--- expense_tracker.py
+++ expense_tracker.py
@@ -3,7 +3,7 @@
 # Store/modify budget through user input, store/modify 
 # imports
 
-income = 0
+income = {}
 expenses = {}
 
 def load_expenses():
@@ -15,9 +15,12 @@ def save_expenses():
     print("Saved!")
 
 def add_expense():
-    expense = input("Enter the name of the monthly expense:\n> ").title()
+    source = input("Enter the name of the monthly expense:\n> ").title()
     value = input("Enter the value of the monthly expense:\n> ")
-    expenses[expense] = value.lstrip("$")
+    expenses[source] = value.lstrip("$")
+    
+def remove_expense():
+    pass
 
 def load_income():
     print("Loading monthly income...")
@@ -26,23 +29,49 @@ def load_income():
 def save_income():
     print("Saving monthly income...")
     print("Saved!")
+    
+def add_income():
+    source = input("Enter the name of the monthly expense:\n> ").title()
+    value = input("Enter the value of the monthly expense:\n> ")
+    income[source] = value.lstrip("$")
+    
+def remove_income():
+    pass
 
 def calc_total():
     total = 0
     for expense in expenses.values():
         total += float(expense)
     leftover_income = income - total
-    print(f"Income is: ${income}")
-    print(f"Expenses list:")
+    print(f"Your monthly income is: ${income}")
+    print(f"Your monthly expenses list:")
+    display_expenses()
+    print(f"\nYour leftover income is: ${leftover_income})
+    if leftover_income < 0:
+          print("Look for ways to cut out expenses to save more money each month!")
+
+def display_expenses():
     for expense, value in expenses.items():
         print(f"{expense}: ${value}")
 
-def display_expenses():
-    pass
-
 def main():
     print("-" * 15 + " Monthly Expense Tracker " + "-" * 15)
+    load_income()
     load_expenses()
+    while True:
+          action = input("""What would you like to do?
+
+1. Add a monthly income source
+2. Remove a monthly income source
+3. Add a monthly expense
+4. Remove a monthly expense
+5. View your monthly income
+6. View your monthly expenses
+7. Exit
+
+>  """
+    if action == "1":
+       add_income()
 
 if __name__ == "__main__":
     main()
